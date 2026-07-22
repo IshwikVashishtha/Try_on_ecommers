53749503e162fa36b68479dae4f8a203a2643c76
ishwik vashishtha <ishwikvashishtha@gmail.com>
2026-07-22 14:35:08 +0530

    upload icon fix

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { GoHeartFill, GoHeart } from "react-icons/go";
-import { HiShoppingBag, HiOutlineShoppingBag } from "react-icons/hi2";
+import { HiShoppingBag, HiOutlineShoppingBag, HiOutlineArrowUpTray } from "react-icons/hi2";
 import { useStore } from "../../context/StoreContext";
 import { IoIosSearch } from "react-icons/io";
 import { useTheme } from "../../context/ThemeContext";
@@ -249,10 +249,7 @@ const Navbar = () => {
               onClick={() => handleImageUpload('image-upload')}
               className="relative text-[20px] text-[var(--text-primary)] cursor-pointer hover:text-[var(--text-secondary)] transition-colors flex items-center"
             >
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M10.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 3a1 1 0 11-2 0 1 1 0 012 0zm5 4a1 1 0 011 1v4a1 1 0 01-1 1h-6a1 1 0 01-1-1V8a1 1 0 011-1h4a1 1 0 011 0zm-6 4a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
-                <path d="M9.5 13a5.5 5.5 0 00-4.906 9.19A3 3 0 105.996 15h8.008a3 3 0 10-1.595 5.19A5.5 5.5 0 009.5 13z" />
-              </svg>
+              <HiOutlineArrowUpTray className="text-[20px]" />
               <input type="file" accept="image/*" id="image-upload" style={{ display: 'none' }} onChange={handleImageChange} />
               
               {userImage && (
@@ -278,10 +275,7 @@ const Navbar = () => {
               onClick={() => handleImageUpload('image-upload-mobile')}
               className="relative text-[20px] text-[var(--text-primary)] cursor-pointer hover:text-[var(--text-secondary)] transition-colors flex items-center"
             >
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path fillRule="evenodd" d="M10.5a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0zM10 3a1 1 0 11-2 0 1 1 0 012 0zm5 4a1 1 0 011 1v4a1 1 0 01-1 1h-6a1 1 0 01-1-1V8a1 1 0 011-1h4a1 1 0 011 0zm-6 4a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd" />
-                <path d="M9.5 13a5.5 5.5 0 00-4.906 9.19A3 3 0 105.996 15h8.008a3 3 0 10-1.595 5.19A5.5 5.5 0 009.5 13z" />
-              </svg>
+              <HiOutlineArrowUpTray className="text-[20px]" />
               <input type="file" accept="image/*" id="image-upload-mobile" style={{ display: 'none' }} onChange={handleImageChange} />
               
               {userImage && (
